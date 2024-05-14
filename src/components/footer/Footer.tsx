e98d628c194bbff0a163ee395f15b27bb4adbf08
mayur kamble <54568942+demonk09@users.noreply.github.com>
2024-05-14 12:04:05 +0530

    final-commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Mayur's</span>
+      <span>© MAYUR Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
