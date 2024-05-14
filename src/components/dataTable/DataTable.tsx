c07abaacd57faefb2975a2d81fc8ce96b3a4625c
mayur kamble <54568942+demonk09@users.noreply.github.com>
2024-05-14 12:16:44 +0530

    second-final-commit

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -32,6 +32,8 @@ const DataTable = (props: Props) => {
   const handleDelete = (id: number) => {
     //delete the item
     // mutation.mutate(id)
+    console.log(id);
+    
   };
 
   const actionColumn: GridColDef = {
